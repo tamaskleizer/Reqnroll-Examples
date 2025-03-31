5c20401a1d0f07bc0808eba63d0903811128dc0f
Tamas Kleizer <takleize@microsoft.com>
2025-03-31 16:03:31 +0200

    add simple separated tests

---
+++ Autofac/SeparatedFeatureSteps/Steps.cs
@@ -0,0 +1,25 @@
+﻿namespace AutofacSeparatedFeatureSteps;
+
+using Reqnroll;
+
+[Binding]
+public class Steps
+{
+    [Given(@"I have steps implemented in another assembly")]
+    public void GivenIStepsImplementedInAnotherAssembly()
+    {
+
+    }
+
+    [When(@"I configure use the right configuration")]
+    public void WhenIConfigureUseTheRightConfiguration()
+    {
+
+    }
+
+    [Then(@"the steps are used correctly")]
+    public void ThenTheStepsAreUsedCorrectly()
+    {
+
+    }
+}
